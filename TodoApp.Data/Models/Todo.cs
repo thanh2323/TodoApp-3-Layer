b2f1f5bb02d2dee261c89faaa065d5eab87fb120
Chi Thanh <nguyenchithanh2213@gmail.com>
2025-09-02 15:23:31 +0700

    Add ToDoRepository

---
+++ TodoApp.Data/Models/Todo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoApp.Data.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+
+        public string Title { get; set; } = string.Empty;
+
+        [MaxLength(1000)]
+        public string? Description { get; set; }
+
+        public bool IsCompleted { get; set; } = false;
+
+        public Priority Priority { get; set; } = Priority.Low;
+
+        public DateTime? DueDate { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        public DateTime? UpdatedAt { get; set; }
+    }
+
+    public enum Priority
+    {
+        Low = 1,
+        Medium = 2,
+        High = 3
+    }
+}
---
+++ TodoApp.Data/TodoDbContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TodoApp.Data.Models;
+
+namespace TodoApp.Data
+{
+    public class TodoDbContext : DbContext
+    {
+        public TodoDbContext(DbContextOptions<TodoDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Todo> Todos { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Todo>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.Title).IsRequired().HasMaxLength(200);
+                entity.Property(e => e.CreatedAt).HasDefaultValueSql("GETDATE()");
+            });
+        }
+    }
+    
+}
